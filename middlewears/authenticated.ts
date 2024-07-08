35d65d846903e5c2746c763d2f5cce2f4e307108
Benedict <benedict.arowo@gmail.com>
2024-07-08 23:20:12 +0300

    refactor: Update error handling in authenticated middleware and auth service

--- middlewears/authenticated.ts
+++ middlewears/authenticated.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { BadrequestError } from "./error";
+import { BadrequestError, UnauthorizedError } from "./error";
 import jwt from "jsonwebtoken";
 import config from "../config";
 import { USER } from "../prisma/db";
@@ -12,20 +12,20 @@ const Authenticated = async (
 	try {
 		const token = req.headers["authorization"]?.split(" ")[1];
 
-		if (!token) return next(new BadrequestError("Invalid token"));
+		if (!token) return next(new UnauthorizedError("Invalid token"));
 		const { id: userId } = jwt.verify(
 			token,
 			config.JWT_KEY as string
 		) as any;
 
-		if (!userId) return next(new BadrequestError("Invalid token"));
+		if (!userId) return next(new UnauthorizedError("Invalid token"));
 		console.log(userId);
 
 		const user = await USER.findUniqueOrThrow({ where: { userId } });
 		(req as any).user = user;
 		next();
 	} catch (err: any) {
-		return next(new BadrequestError("Invalid user"));
+		return next(new UnauthorizedError("Unauthorized"));
 	}
 };
 
--- services/auth.service.ts
+++ services/auth.service.ts
@@ -34,7 +34,7 @@ class AuthService {
 			};
 		} catch (err: any) {
 			console.log(err);
-			throw new UnauthorizedError("Authentication Failed");
+			throw new BadrequestError("Authentication failed");
 		}
 	};
 
