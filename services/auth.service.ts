ab934446a9ad65b6dec68dc8f89d6d7382c34522
Benedict <benedict.arowo@gmail.com>
2024-07-08 19:15:15 +0300

    refactor: Update error handling in AuthService registration method

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -34,7 +34,7 @@ class AuthService {
 			};
 		} catch (err: any) {
 			console.log(err);
-			throw new UnauthorizedError("Registration unsuccessful");
+			throw new UnauthorizedError("Authentication Failed");
 		}
 	};
 
@@ -84,7 +84,7 @@ class AuthService {
 				throw new ValidationError([
 					{ field: "email", message: "Email already exists" },
 				]);
-			throw new BadrequestError("Authentication failed");
+			throw new BadrequestError("Registration unsuccessful");
 		}
 	};
 
